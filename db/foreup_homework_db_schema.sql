661fb776a2d7936689f0df16bb0b0429fe7b1c33
tmyree68 <troymyree86.gmail.com>
2024-09-19 15:31:58 -0700

    include database schema

---
+++ db/foreup_homework_db_schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE `foreup_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- foreup_db.foreup_homework definition
+
+CREATE TABLE `foreup_homework` (
+                                   `character_name` varchar(100) DEFAULT NULL,
+                                   `character_image_url` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
